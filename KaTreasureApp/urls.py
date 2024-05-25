19a982150225be92b3a1be785a39d75c37864d60
Celso M. Puerto Jr. <isoygwapo84@gmail.com>
2024-05-25 20:19:58 +0800

    add pages

--- KaTreasureApp/urls.py
+++ KaTreasureApp/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'KaTreasureApp'
-
 urlpatterns = [
-    path('base/', views.base, name='base'),
-    path('contactus/', views.contactus, name='contactus'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('logout/', views.logout, name='logout'),
-    path('forgot_password', views.forgot_password, name='forgot_password'),
-    path('', views.home, name="home"),
+    path("", views.index, name="index"),
 ]
--- KaTreasureEH/settings.py
+++ KaTreasureEH/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-=!wkgp-egk+m3qq2+(2+!63_lreecswg@hb#(m2z+x5c=1c(0&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
