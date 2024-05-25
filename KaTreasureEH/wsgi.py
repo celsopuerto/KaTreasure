d22e0f8d1ddee2c9ae6214db94e4690e6ce4acd2
Celso M. Puerto Jr. <isoygwapo84@gmail.com>
2024-05-25 19:42:23 +0800

    first commit

---
+++ KaTreasureEH/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for KaTreasureEH project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KaTreasureEH.settings')
+
+application = get_wsgi_application()
